eb001e9632d27e768b91776ef543e134b851a0d9
Trương Quang Quyền <QuangQuyen1305@gmail.com>
2024-11-20 20:43:32 +0700

    n

---
+++ dsc-backend/DAO/CreateTournamentDAO.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace dsc_backend.DAO
+{
+    public class CreateTournamentDAO
+    {
+
+            public int SportId { get; set; }
+
+
+            public int LevelId { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime StartDate { get; set; }
+
+
+            public DateTime EndDate { get; set; }
+
+        public DateTime startTime { get; set; }
+        
+
+            public DateTime RegistrationDeadline { get; set; }
+
+
+            public int numberOfParticipants { get; set; }
+
+            public int UserId { get; set; }
+
+        public int teamSize { get; set; }
+            public string? note { get; set; }
+
+            public string? location { get; set; }
+
+
+        }
+}
